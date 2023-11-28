95558750957673f0d1d5c715cd855b3ed553747d
Johnson <ogbonnayajprof2015@gmail.com>
2023-11-28 12:34:09 +0100

    fix styling

--- my-react-app/src/components/Products/Products.jsx
+++ my-react-app/src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import EachProduct from "../EachProduct/EachProduct";
 const Products = ({ data, ff }) => {
   console.log(ff, data);
   return (
-    <div className="flex flex-col flex-wrap mx-auto md:w-4/5 items-center justify-center md:justify-between md:flex-row sm2:flex-row">
+    <div className="flex flex-col flex-wrap mx-auto md:w-4/5 items-center justify-center md:flex-row sm2:flex-row">
       {ff.length !== 0
         ? ff.map((product) => <EachProduct name={product.name} />)
         : data.map((product) => <EachProduct name={product.name} />)}
